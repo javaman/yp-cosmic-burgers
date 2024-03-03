2e576ae883e728d7756842ab95e18e8fd81ed2f9
Maxim Krylov <b0wser@mail.ru>
2024-03-03 17:22:38 +0300

    Второй спринт, первое задание, первый подход

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import AppHeader from './components/app-header/app-header';
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
 import './App.css';
@@ -7,37 +6,61 @@ import BurgerConstructor from './components/burger-constructor/burger-constructo
 import Modal from './components/modal/modal';
 import IngredientDetails from './components/ingredient-details/ingredient-details';
 import OrderDetails from './components/order-details/order-details';
+import BurgerConstructorContext from './services/burger-constructor-context';
+import BurgerItem from './types/burger-types';
+import { TBurgerConstructorContext } from './services/burger-constructor-context';
+import { BUN, API_URL, SUBMIT_URL } from './constants';
 
-const API_URL = "https://norma.nomoreparties.space/api/ingredients";
 
-type Item = {
-  _id: string;
-  name: string;
-  type: string;
-  proteins: number;
-  fat: number;
-  carbohydrates: number;
-  calories: number;
-  price: number;
-  image: string;
-  image_mobile: string;
-  image_large: string;
-  __v: number;
-};
 
 function App() {
   const [modalOpen, setModalOpen] = React.useState(false);
-  const [item, setItem] = React.useState<Item>();
-  const [ingredients, setIngredients] = React.useState<Item[]>([]);
+  const [item, setItem] = React.useState<BurgerItem>();
+  const [ingredients, setIngredients] = React.useState<BurgerItem[]>([]);
+  const [burgerConstructorContext, setBurgerConstructorContext] = React.useState<TBurgerConstructorContext>({bun: BUN, items: [], orderNumber: -1});
 
-  function showItem(i: Item) {
+  function showItem(i: BurgerItem) {
+    // Это я сделал, чтобы динамики добавить, понятное дело что в финале этого не будет и потестировать
+    if (i.type === 'bun') {
+      setBurgerConstructorContext({
+        ...burgerConstructorContext,
+        bun: i
+      });
+    } else {
+      setBurgerConstructorContext({
+        ...burgerConstructorContext,
+        items:  [...burgerConstructorContext.items, i]
+      });
+    }
     setItem(i);
     setModalOpen(true);
   };
 
   function submitOrder() {
-    setModalOpen(true);
-    setItem(undefined);
+    fetch(SUBMIT_URL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ingredients: [...burgerConstructorContext.items.map(i => i._id), burgerConstructorContext.bun?._id, burgerConstructorContext.bun?._id] 
+      })
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Все пропало!");
+      }
+    }).then(response => {
+      setBurgerConstructorContext({
+        ...burgerConstructorContext,
+        orderNumber: response.order.number
+      });      
+      setModalOpen(true);
+      setItem(undefined);
+    })
+    .catch(error => alert(error));
   }
 
   React.useEffect(() => {
@@ -47,26 +70,25 @@ function App() {
           return response.json();
         } else {
           alert("Все пропало!");
-          return {data: []};
+          return { data: [] };
         }
       }).then(response => setIngredients(response.data))
       .catch(error => alert("Все пропало!"));
   }, []);
 
   return (
-    <>
+    <BurgerConstructorContext.Provider value={burgerConstructorContext}>
       <div className='wrapper'>
         <header className='header'><AppHeader /><h1 className='text text_type_main-large m-8'>Соберите бургер</h1></header>
-        <aside className='sidebarl'><BurgerIngredients items={ingredients} onItemClicked={(item: Item) => showItem(item)} /></aside>
-        <aside className='sidebarr'><BurgerConstructor items={ingredients} submitOrder={() => submitOrder()} /></aside>
-
+        <aside className='sidebarl'><BurgerIngredients items={ingredients} onItemClicked={(item: BurgerItem) => showItem(item)} /></aside>
+        <aside className='sidebarr'><BurgerConstructor submitOrder={() => submitOrder()} /></aside>
       </div>
       {modalOpen &&
         <Modal closeModal={() => setModalOpen(false)} title={item ? "Детали ингредиента" : ""}>
           {item ? <IngredientDetails ingredient={item} /> : <OrderDetails />}
         </Modal>
       }
-    </>
+    </BurgerConstructorContext.Provider>
   );
 }
 
--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -4,19 +4,38 @@ import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-de
 import styles from './burger-constructor.module.css'
 import Types from '../../prop-types';
 import PropTypes from 'prop-types';
+import BurgerConstructorContext from '../../services/burger-constructor-context';
+
+const BurgerConstructor = ({ submitOrder }) => {
+    const {bun, items} = React.useContext(BurgerConstructorContext);
+
+    function computeTotal() {
+        return {total: bun.price * 2 + items.map(i => i.price).reduce((a, b) => a + b, 0)};
+    }
+
+    function computeTotalReducer(state, action) {
+        return computeTotal();
+    }
+
+    const [total, dispatchTotal] = React.useReducer(computeTotalReducer, {total: 0}, computeTotal);
+
+    // Простите! Я только учусь.
+    React.useEffect(() => {
+        dispatchTotal();
+    }, [bun, items]);
 
 
-
-const BurgerConstructor = ({ items, submitOrder }) => {
     return (
         <div>
             <div className={styles.listScroll}>
                 <ul className={styles.c}>
+                    <li><ConstructorElement thumbnail={bun.image_mobile} text={bun.name} price={bun.price} type='top' /></li>
                     {items.map((i, idx) => (<li key={i._id}><ConstructorElement thumbnail={i.image_mobile} text={i.name} price={i.price} type={idx == 0 ? 'top' : idx == items.length - 1 ? 'bottom' : undefined} /></li>))}
+                    <li><ConstructorElement thumbnail={bun.image_mobile} text={bun.name} price={bun.price} type='bottom' /></li>
                 </ul>
             </div>
             <div className={styles.buttonFooter}>
-                <span className='text text_type_digits-medium mr-2'>610</span>
+                <span className='text text_type_digits-medium mr-2'>{total.total}</span>
                 <CurrencyIcon type='primary' />
                 <Button htmlType="button" type="primary" size="medium" extraClass='ml-4' onClick={submitOrder}>
                     Оформить заказ
@@ -27,7 +46,6 @@ const BurgerConstructor = ({ items, submitOrder }) => {
 }
 
 BurgerConstructor.propTypes = {
-    items: PropTypes.arrayOf(Types.Item),
     submitOrder: PropTypes.func
 }
 
